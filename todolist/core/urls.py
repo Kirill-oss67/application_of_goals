41a17718a23b306102d5fd15f9a9edeaa09d9e64
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-10-17 23:07:46 +0300

    correct path for sign-up

--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignupView, LoginView, ProfileView, UpdatePasswordView
 
 urlpatterns = [
-    path('signup', SignupView.as_view(), name='signup'),
+    path('sign-up', SignupView.as_view(), name='signup'),
     path('login', LoginView.as_view(), name='login'),
     path('profile', ProfileView.as_view(), name='update-retrieve-destroy-user'),
     path('update_password', UpdatePasswordView.as_view(), name='update_password')
