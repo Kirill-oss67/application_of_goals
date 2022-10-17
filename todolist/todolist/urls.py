ea8260164138556972b57a92e4c59c720307dc0a
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-10-17 23:00:06 +0300

    start adding vk auth

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("core/", include("core.urls"))
+    path("core/", include("core.urls")),
+    path('oauth/', include("social_django.urls", namespace='social'))
 ]
