552473998d87bf278f57a414242bfbe3a2c441f5
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-10-18 17:52:14 +0300

    test commit

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -59,8 +59,8 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class UpdatePasswordSerializer(serializers.Serializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    old_password = PasswordField(read_only=True)
-    new_password = PasswordField(read_only=True)
+    old_password = PasswordField(required=True)
+    new_password = PasswordField(required=True)
 
     def validate(self, attrs: dict):
         if not (user := attrs['user']):
