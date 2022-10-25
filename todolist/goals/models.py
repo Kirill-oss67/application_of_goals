2c55f1c12b035ad7c1b847f3d6d428442b6bf435
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-10-25 19:14:07 +0300

    make model of Goal

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -23,8 +23,30 @@ class GoalCategory(BaseModel):
     def __str__(self):
         return self.title
 
+
 class Goal(BaseModel):
     class Meta:
         verbose_name = "Цель"
         verbose_name_plural = "Цели"
 
+    class Status(models.IntegerChoices):
+        to_do = 1, "К выполнению"
+        in_progress = 2, "В процессе"
+        done = 3, "Выполнено"
+        archived = 4, "Архив"
+
+    class Priority(models.IntegerChoices):
+        low = 1, "Низкий"
+        medium = 2, "Средний"
+        high = 3, "Высокий"
+        critical = 4, "Критический"
+
+    title = models.CharField(verbose_name='Название', max_length=255)
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+
+    status = models.PositiveSmallIntegerField(
+        verbose_name="Статус", choices=Status.choices, default=Status.to_do
+    )
+    priority = models.PositiveSmallIntegerField(
+        verbose_name="Приоритет", choices=Priority.choices, default=Priority.medium
+    )
