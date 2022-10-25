1e154df93708acc479ff4d2513ec080dc4195b15
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-10-25 12:12:27 +0300

    Creating in the file models.py the model for the category.

---
+++ todolist/goals/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+from todolist.core.models import User
+
+
+class GoalCategory(models.Model):
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    created = models.DateTimeField(verbose_name="Дата создания")
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Когда объект только создается, у него еще нет id
+            self.created = timezone.now()  # проставляем дату создания
+        self.updated = timezone.now()  # проставляем дату обновления
+        return super().save(*args, **kwargs)
--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     'social_django',
-    'core'
+    'core',
+    'goals'
 ]
 
 MIDDLEWARE = [
