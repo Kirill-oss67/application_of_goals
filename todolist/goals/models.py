d0d3b95845ef27467a408acb05ae4bae912f31b8
Kirill-oss67 <Yakovkirill-roslavl@yandex.ru>
2022-10-26 18:20:37 +0300

    make GoalComment model

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -68,3 +68,26 @@ class Goal(BaseModel):
         choices=Priority.choices,
         default=Priority.medium
     )
+
+    def __str__(self):
+        return self.title
+
+
+class GoalComment(BaseModel):
+    user = models.ForeignKey(User, verbose_name="Автор",
+                             on_delete=models.PROTECT,
+                             related_name='comments',
+                             null=True)
+    goal = models.ForeignKey(Goal, verbose_name="Цель",
+                             on_delete=models.CASCADE,
+                             related_name='comments',
+                             null=True)
+    text = models.TextField(verbose_name='Текст')
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+    def __str__(self):
+        return self.text
+
--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -10,7 +10,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env_example'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env_deploy'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
