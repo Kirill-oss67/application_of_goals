890779e814af8122bfea3b8e6d01c1021b8b6e4e
Kirill-oss67 <yakovkirill-roslavl@yandex.ru>
2022-11-03 16:16:22 +0300

    board's model created

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -11,10 +11,23 @@ class BaseModel(models.Model):
         abstract = True
 
 
+class Board(BaseModel):
+    class Meta:
+        verbose_name = "Доска"
+        verbose_name_plural = "Доски"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+
 class GoalCategory(BaseModel):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+    board = models.ForeignKey(
+        Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories"
+    )
 
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
@@ -90,3 +103,31 @@ class GoalComment(BaseModel):
 
     def __str__(self):
         return self.text
+
+
+class BoardParticipant(BaseModel):
+    class Meta:
+        unique_together = ("board", "user")
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
+
+    class Role(models.IntegerChoices):
+        owner = 1, "Владелец"
+        writer = 2, "Редактор"
+        reader = 3, "Читатель"
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name="Роль", choices=Role.choices, default=Role.owner
+    )
